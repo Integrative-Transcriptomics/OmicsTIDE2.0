2985456a3a8b17c01e3074f9265e185f1388fe0a
ThHarbig <theresa_ha@hotmail.de>
2022-07-21 15:46:24 +0200

    remove unused import

--- src/HomeTab.jsx
+++ src/HomeTab.jsx
@@ -3,7 +3,6 @@ import {Button} from "@mui/material";
 import abstract from "./graphical_abstract.png"
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import axios from "axios";
 
 function HomeTab(props) {
     return <div style={{margin: 10}}>
